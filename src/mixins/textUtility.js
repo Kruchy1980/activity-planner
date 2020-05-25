893a93f74f04fd9330819aa6826c8df09d770e72
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-05-25 23:11:50 +0200

    Deleted file and once more trying to deploy

--- src/mixins/textUtility.js
+++
@@ -1,9 +0,0 @@
-export default {
-    methods: {
-        textUtility_capitalizedWord(word) {
-            if (word && typeof word === "string") {
-                return word.charAt(0).toUpperCase() + word.slice(1);
-            }
-        }
-    }
-}
